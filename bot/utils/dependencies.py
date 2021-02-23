51d7252a9a773dc69aef18ab444008adf3bd625f
Bluenix <bluenixdev@gmail.com>
2021-02-23 02:28:34 +0100

    Implement __call__ on Depends and make dependency decorator
    No longer requiring Depends(func), also allows the use of the new subclass nicely because it's handled in the decorator.

--- bot/utils/dependencies.py
+++ bot/utils/dependencies.py
@@ -1,15 +1,40 @@
 from fastapi import Request
+from fastapi.params import Depends  # The real Depends class
 from utils import ConnectionUtil
 
 
+class CallableDepends(Depends):
+    """Depends with __call__ implemented as a shortcut to Depends.dependency()."""
+
+    def __call__(self, *args, **kwargs):
+        return self.dependency(*args, **kwargs)
+
+
+def dependency(func=None, *, use_cache: bool = True):
+    def decorator(wrap):
+        return CallableDepends(dependency=wrap, use_cache=use_cache)
+
+    # Allow decorator to be used without calling it
+    if callable(func):
+        return decorator(func)
+
+    return decorator
+
+
+@dependency()
 async def get_app(request: Request):
+    """Dependency to get the serving app."""
     return request.state.app
 
 
+@dependency()
 async def get_bot(request: Request):
+    """Dependency to get the running bot."""
     # In case how we get the app is changed
     return (await get_app(request)).bot
 
 
-async def get_conn(request: Request):
+@dependency()
+async def get_conn(request: Request) -> ConnectionUtil:
+    """Dependency to get a ConnectionUtil connection to work with."""
     return ConnectionUtil((await get_bot(request)).pool)
