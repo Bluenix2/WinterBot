4edf197825e40b16f71bba7a8b1f251b6cb7273e
Bluenix <bluenixdev@gmail.com>
2021-02-26 02:46:27 +0100

    Add eightball command and management routes

---
+++ bot/extensions/misc/__init__.py
@@ -0,0 +1,26 @@
+from extensions.misc import eight_ball
+
+
+async def init(bot):
+    """Initialize the necessary database features."""
+    async with bot.pool.acquire() as conn:
+        # 8ball
+        await conn.execute("""CREATE SEQUENCE eightball_sqc;""")
+        await conn.execute("""
+            CREATE TABLE eightball (
+                id SMALLINT PRIMARY KEY DEFAULT NEXTVAL('eightball_sqc'),
+                response TEXT NOT NULL,
+                weight SMALLINT NOT NULL DEFAULT 1
+            );
+        """)
+        await conn.execute("""ALTER SEQUENCE eightball_sqc OWNED BY eightball.id;""")
+
+        await conn.execute("""CREATE INDEX eightball_weight_idx ON eightball (id, weight);""")
+
+
+def setup(bot):
+    if bot.init:
+        return bot.loop.run_until_complete(init(bot))
+
+    bot.add_command(eight_ball.eightball)
+    bot.app.include_router(eight_ball.api)
---
+++ bot/extensions/misc/eight_ball.py
@@ -0,0 +1,77 @@
+from typing import List, Optional
+
+from discord.ext import commands
+from fastapi import APIRouter
+from pydantic import BaseModel
+from utils import ConnectionUtil, get_conn
+
+
+class PartialAnswer(BaseModel):
+    """Represents the model for editing an 8ball answer."""
+    response: Optional[str]
+    weight: Optional[int]
+
+
+class NewAnswer(PartialAnswer):
+    """Represents the mode for inserting a new 8ball answer."""
+    # Override the Optional typing
+    response: str
+    weight: int
+
+
+class Answer(NewAnswer):
+    """Represents an already inserted 8ball answer, is usually returned."""
+    id: int
+
+
+api = APIRouter(prefix='/8ball')
+
+
+@api.get('/')
+async def get_random_response(conn: ConnectionUtil = get_conn) -> str:
+    # Weighted random selection
+    return await conn.fetchval('SELECT response FROM eightball ORDER BY RANDOM()*weight;')
+
+
+@api.get('/answers', response_model=List[Answer])
+async def get_answers(conn: ConnectionUtil = get_conn) -> List[Answer]:
+    # FastAPI converts it into a list of Answers
+    return [record for record in await conn.fetch('SELECT * FROM eightball;')]
+
+
+@api.post('/answers', response_model=Answer)
+async def add_answer(answer: NewAnswer, conn: ConnectionUtil = get_conn) -> Answer:
+    # FastAPI handles convering it into an Answer
+    return await conn.fetchrow(
+        'INSERT INTO eightball (response, weight) VALUES ($1, $2) RETURNING *;',
+        answer.response, answer.weight
+    )
+
+
+@api.get('/answers/{answer_id}', response_model=Answer)
+async def get_answer(answer_id: int, conn: ConnectionUtil = get_conn) -> Answer:
+    return await conn.fetchrow(
+        'SELECT id, response, weight FROM eightball WHERE id=$1', answer_id
+    )
+
+
+@api.patch('/answers/{answer_id}', response_model=Answer)
+async def edit_answer(
+    answer_id: int, answer: PartialAnswer, conn: ConnectionUtil = get_conn
+) -> Answer:
+    return await conn.fetchrow("""
+        UPDATE eightball SET
+            response=COALESCE($2, response),
+            weight=COALESCE($3, weight)
+        WHERE id=$1 RETURNING *;
+    """, answer_id, answer.response, answer.weight)
+
+
+@api.delete('/answers/{answer_id}', response_model=Answer)
+async def delete_answer(answer_id: int, conn: ConnectionUtil = get_conn) -> Answer:
+    return await conn.fetchrow('DELETE FROM eightball WHERE id=$1 RETURNING *;', answer_id)
+
+
+@commands.command(name='8ball')
+async def eightball(ctx, *, question: str = None):
+    await ctx.send(await get_random_response(ctx))
